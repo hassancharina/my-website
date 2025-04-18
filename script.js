e5a8a8b257e64852efa2ea099fccb9f558228f97
Hassan Charina <hassancharina@MacBook-von-Hassan.fritz.box>
2025-04-19 01:44:20 +0200

    Redesign Hero section with typing effect and improved layout

--- script.js
+++ script.js
@@ -34,3 +34,48 @@ updateDateTime();
 
 // Optional: Update every second
 setInterval(updateDateTime, 1000);
+const typingText = document.querySelector(".typing-text");
+const phrases = ["Computer Engineering Student", "Web Developer", "Tech Explorer"];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
+
+function loop() {
+  isEnd = false;
+  typingText.innerHTML = currentPhrase.join("");
+
+  if (i < phrases.length) {
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j]);
+      j++;
+      typingText.innerHTML = currentPhrase.join("");
+    }
+
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop();
+      j--;
+      typingText.innerHTML = currentPhrase.join("");
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = [];
+      isDeleting = false;
+      i++;
+      if (i === phrases.length) {
+        i = 0;
+      }
+    }
+  }
+
+  const speed = isEnd ? 2000 : isDeleting ? 50 : 100;
+  setTimeout(loop, speed);
+}
+
+loop();
