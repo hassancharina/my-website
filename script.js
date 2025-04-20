df80653225b83289990a67a69c3fabe6978591ac
Hassan Charina <hassancharina@MacBook-von-Hassan.fritz.box>
2025-04-20 10:46:14 +0200

    debug: test burger menu click event with console log

--- script.js
+++ script.js
@@ -103,5 +103,6 @@ const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
 
 menuToggle.addEventListener("click", () => {
+  console.log("menu clicked");
   navLinks.classList.toggle("show");
 });
